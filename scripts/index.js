11c3003b60a48089a7bb001e687770d141659f38
scal3 <cool.goga501@gmail.com>
2021-12-15 21:32:57 +0300

    fix/update method validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,50 @@
 const taskContainer = document.querySelector('.main__todo-container')
 const taskTemplate = document.querySelector('#one').content
-const taskForm = document.querySelector('.main__form')
-const taskInput = document.querySelector('.main__form-input')
-const taskAddButton = document.querySelector('.main__form-button')
+const taskForm  = document.forms.taskForm
+const taskInput = taskForm.task
+const taskAddButton = taskForm.submit
 
 
 //  Оботчик кнопки add. Клонируем темплэйт, устанавливаем значение из инпута и апендим в контейнер
 const handleTaskSubmit = () => {
     const task = taskTemplate.querySelector('.task').cloneNode(true)
     task.querySelector('.task__title').textContent = taskInput.value
-    taskContainer.append(task)
-    disabledSubmit(taskAddButton)
+    taskContainer.prepend(task)
+    disabledButton(taskAddButton)
+    taskForm.reset()
 }
 
-//  Функция дизэйбла любой кнопки
-const disabledSubmit = (button) => {
+//  Функция дизэйбла кнопки
+const disabledButton = (button) => {
     button.setAttribute('disabled', true)
 }
 
-//  Слушатель на кнопку удаления
-window.addEventListener('click', (event) => {
+//  Функция включения кнопки
+const enabledButton = (button) => {
+    button.removeAttribute('disabled', true)
+}
+
+//  Оботчик кнопки удаления задачи
+const handleTaskDelete = (event) => {
     if(event.target.classList.contains('task__button')) {
         event.target.closest('.task').remove();
     }
-})
+}
 
-//  Слушатель на сабмит формы
+//  Оботчик состояния сабмита
+const handleButtonState = () =>{
+    if(taskInput.value.length === 0) {
+        disabledButton(taskAddButton)
+    } else {
+        enabledButton(taskAddButton)
+    }
+}
+
+
 taskForm.addEventListener('submit', (event) => {
     event.preventDefault()
     handleTaskSubmit()
-    taskForm.reset()
 })
-
-//  Слушатель на инпут для дизэйбла сабмита
-window.addEventListener('keyup', (event) => {
-    if(event.target.classList.contains('main__form-input')) {
-        if(event.target.value.length <= 0) {
-            const submit = event.target.closest('.main__form').querySelector('.main__form-button')
-            submit.setAttribute('disabled', true)
-        }   else {
-            const submit = event.target.closest('.main__form').querySelector('.main__form-button')
-            submit.removeAttribute('disabled', true)
-        }
-    }
-})
-
-//  Слушатель на документ. При загрузке контента отключаем кнопку
-document.addEventListener("DOMContentLoaded", disabledSubmit(taskAddButton))
+window.addEventListener('click', (event) => handleTaskDelete(event))
+taskForm.addEventListener('input', handleButtonState)
+document.addEventListener("DOMContentLoaded", disabledButton(taskAddButton))
